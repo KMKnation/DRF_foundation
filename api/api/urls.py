fe3a90097741c81eab154b921c430a1af9bb55de
Mayur Kanojiya <kanojiyamayur@gmail.com>
2020-05-18 18:38:33 +0530

    feat: Added frontend module

--- api/api/urls.py
+++ api/api/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('check_server', index, name="check")
+    path('check_server', index, name="check"),
+    path('home', include('website.urls'))
 ]
